aad09db257830e70671076c9415cf29e9631e430
Bells307 <skyheart307@yandex.ru>
2025-12-12 15:48:51 +0300

    fix

--- nvim/default.nix
+++ nvim/default.nix
@@ -1,19 +1,9 @@
-{ pkgs, ... }:
-
-{
+{...}: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    extraPackages = with pkgs; [
-      stylua
-      rust-analyzer
-      rustfmt
-      nil
-      jq
-      alejandra
-    ];
   };
 
   home.file.".config/nvim".source = ./nvim;
